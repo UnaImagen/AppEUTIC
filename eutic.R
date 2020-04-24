a3b120dadb3a2292f67a0993e3a097a1b1b3ceed
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2020-04-23 23:17:21 -0300

    ajusta ruta a nuevo proj
    parsea
    - uso de celular (cambia por una única variable que combina celulares comúnes con smart phones)
    - uso de pc
    - uso de internet
    - frecuencia de uso de internet

--- eutic.R
+++ eutic.R
@@ -7,7 +7,7 @@ library(tidyverse)
 
 # Carga datos -------------------------------------------------------------
 eutic <- haven::read_sav(
-   file = here::here("DataBases/EUTIC/BASE HOGARES Y PERSONAS EUTIC 2016.sav")
+   file = here::here("BASE HOGARES Y PERSONAS EUTIC 2016.sav")
 ) %>%
    dplyr::rename_all(
       .funs = tolower
@@ -17,7 +17,7 @@ eutic <- haven::read_sav(
 # Construye objeto para el App --------------------------------------------
 eutic %>%
    # filter(
-   #    p23_1 == 0
+   #    p23_1 == 3
    # ) %>%
    dplyr::transmute(
       nper = dplyr::row_number(),
@@ -26,21 +26,49 @@ eutic %>%
       edad = base::as.integer(p13),
 
       ## Uso de celular
-      uso_celular = forcats::as_factor(p23),
+      uso_celular_comun = forcats::as_factor(p23),
       uso_smart_phone = forcats::as_factor(p23_1),
-      uso_smart_phone = forcats::fct_collapse(
-         .f = uso_smart_phone,
-         "Sí" = "Sí",
-         "No" = "No",
-         other_level = "Ns/Nc"
+      uso_celular = dplyr::case_when(
+         uso_celular_comun == "No" & uso_smart_phone == "0" ~ "No utiliza",
+         (uso_celular_comun == "Sí" & uso_smart_phone == "No") | (uso_celular_comun == "Sí" & uso_smart_phone == "No sabe") ~ "Utiliza celular",
+         uso_celular_comun == "Sí" & uso_smart_phone == "Sí" ~ "Utiliza Smart Phone",
+         TRUE ~ NA_character_
       ),
 
       ## Uso de tablet
       uso_tablet = forcats::as_factor(p26),
 
+      ## Uso de PC
+      uso_pc = forcats::as_factor(p27),
+
+      ## Uso Internet
+      uso_internet = forcats::as_factor(p37),
+      frecuencia_uso_internet = forcats::as_factor(p40),
+      frecuencia_uso_internet = forcats::fct_recode(
+         .f = frecuencia_uso_internet,
+         "Entre una y tres veces al día" = "Diariamente,entre una y tres veces al dia",
+         "Cuatro o más veces al día" = "Diariamente,cuatro o mas veces al dia",
+         "Al menos una vez a la semana" = "Al menos una vez a la semana pero no todos los días",
+         "Al menos una vez al mes" = "Al menos una vez al mes pero no todas las semanas",
+         "No recuerda" = "No recuerda o muy irregularmente (no leer)",
+         "No utiliza" = "0"
+      ),
+      frecuencia_uso_internet = forcats::fct_relevel(
+         .f = frecuencia_uso_internet,
+         "Cuatro o más veces al día",
+         "Entre una y tres veces al día",
+         "Al menos una vez a la semana",
+         "Al menos una vez al mes",
+         "Menos de una vez al mes",
+         "No utiliza",
+         "No recuerda"
+      )
 
    )
 
+xtabs(~p23 + p23_1, data = eutic, addNA = TRUE)
+table(eutic$p40, useNA = "always")
+levels(.Last.value$frecuencia_uso_internet)
 
 x %>%
    group_by(
@@ -52,9 +80,6 @@ x %>%
    y = ~n,
    type = "bar"
 )
-
-table(eutic$p26, useNA = "always")
-levels(x$uso_smart_phone)
 
 #===============#
 #### THE END ####
