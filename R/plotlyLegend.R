fe112dfa4ce1d2f5256e94774ecd865454764a6e
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-03-07 19:05:33 -0300

    sets colors and bg-colors

--- R/plotlyLegend.R
+++ R/plotlyLegend.R
@@ -2,11 +2,14 @@ plotlyLegend <- function(.p, y = -0.30) {
    .p %>%
       plotly::layout(
          legend = base::list(
-            bgcolor = "#e2e2e2",
+            bgcolor = "#2c3e50",
             orientation = "h",
             yanchor = "bottom",
             xanchor = "left",
-            y = y
+            y = y,
+            font = base::list(
+               color = "#f5f5f5"
+            )
          )
       )
 }
